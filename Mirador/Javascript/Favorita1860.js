3f52a92b5b89e9652dbd124c6dca52fde109096b
Corrado Consiglio <114939477+corrado877@users.noreply.github.com>
2023-09-16 13:17:44 +0200

    Add files via upload
    Mirador Favorita

---
+++ Mirador/Javascript/Favorita1860.js
@@ -0,0 +1,15 @@
+var miradorInstance4 = Mirador.viewer({
+    "id": "mirador_la_favorita_1860",
+    "manifests": {
+        "https://corrado877.github.io/ManifestsLaFavorita/manifests/DailaManifest.json": {
+        "provider": "Internet Archive"
+        }
+    },
+    "windows": [
+        {
+        "loadedManifest": "https://corrado877.github.io/ManifestsLaFavorita/manifests/DailaManifest.json",
+        "canvasIndex": 0,
+        "thumbnailNavigationPosition": 'far-bottom'
+        }
+    ]
+    });    
